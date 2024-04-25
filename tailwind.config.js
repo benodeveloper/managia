ab88543d61106c552a10958f34c5a3eae3bfe147
listoun <hassan0outline0@gmail.com>
2024-04-25 18:09:25 +0100

    setup dashboard with tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+module.exports = {
+    content: [path.resolve(__dirname, 'resources/templates/**/*.html')],
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
